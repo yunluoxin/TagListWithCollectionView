f689aa62dd79ebfb1cb6b62620b1868501d675bc
ZhangXiaodong <Xiaodong@zhangxingdeimac.lan>
2016-09-13 21:32:10 +0800

    Initial Commit

---
+++ TagWithCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TagWithCollectionView
+//
+//  Created by ZhangXiaodong on 16/9/13.
+//  Copyright © 2016年 ZhangXiaodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TagWithCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TagWithCollectionView
+//
+//  Created by ZhangXiaodong on 16/9/13.
+//  Copyright © 2016年 ZhangXiaodong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
